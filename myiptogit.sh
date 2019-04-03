86b369b10b92da376b18a3863064c395e3790eec
Mosl1k <mosol.07@mail.ru>
2019-04-03 08:06:02 -0700

    nip

--- myiptogit.sh
+++ myiptogit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget -O - -q icanhazip.com > /home/moslik/mos/mosl1k.github.io/ismyip.txt 
-`date` > /home/moslik/mos/mosl1k.github.io/lasttime.txt 
+echo `date` > /home/moslik/mos/mosl1k.github.io/lasttime.txt 
 cd /home/moslik/mos/mosl1k.github.io/
 git add .
 git commit -am nip
