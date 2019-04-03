7a8caeccde9cce2a652149b8146d8d9970a36da0
Mosl1k <mosol.07@mail.ru>
2019-04-02 18:52:05 -0700

    ip

--- myiptogit.sh
+++ myiptogit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-a=`wget -O - -q icanhazip.com` && echo $a > ismyip.txt && git add . && git commit -am "ip" && git push ;
+a=`wget -O - -q icanhazip.com`
+echo $a > ismyip.txt 
+git add . 
+git commit -am "ip" 
+git push 
 
 
