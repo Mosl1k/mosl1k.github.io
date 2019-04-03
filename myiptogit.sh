7bc913b8ac30ebb636feccb70b59a07139d58ca7
Mosl1k <mosol.07@mail.ru>
2019-04-03 07:25:24 -0700

    nip

--- myiptogit.sh
+++ myiptogit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo wget -O - -q icanhazip.com >> /home/moslik/mos/mosl1k.github.io/ismyip.txt 
+wget -O - -q icanhazip.com >> ~/mos/mosl1k.github.io/ismyip.txt 
 gnome-terminal -- git add ~/mos/mosl1k.github.io/ismyip.txt 
 gnome-terminal -- git commit -am nip
 gnome-terminal -- git push 
