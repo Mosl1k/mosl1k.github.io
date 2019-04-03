c8e62582108d4d4237d653479a2d6a3fb6dd1ab9
Mosl1k <mosol.07@mail.ru>
2019-04-03 07:59:01 -0700

    nip

--- myiptogit.sh
+++ myiptogit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O - -q icanhazip.com >> /home/moslik/mos/mosl1k.github.io/ismyip.txt 
+wget -O - -q icanhazip.com > /home/moslik/mos/mosl1k.github.io/ismyip.txt 
 
 cd /home/moslik/mos/mosl1k.github.io/
 git add .
