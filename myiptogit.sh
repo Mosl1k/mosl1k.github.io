4dbbf2c532c7b84dfb021542d8f2f67133c3bb08
Mosl1k <mosol.07@mail.ru>
2019-04-04 10:05:01 -0700

    nip

--- myiptogit.sh
+++ myiptogit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -O - -q icanhazip.com > /home/moslik/mos/mosl1k.github.io/ismyip.txt 
-echo `date` > /home/moslik/mos/mosl1k.github.io/lasttime.txt 
 cd /home/moslik/mos/mosl1k.github.io/
+wget -O - -q icanhazip.com > ismyip.txt 
+echo `date` >> ismyip.txt 
 git add .
 git commit -am nip
 git push 
