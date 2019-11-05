37ce606e648085521108d0ab00f5cc1bdecad347
Konstantin <38739711+Mosl1k@users.noreply.github.com>
2019-11-05 21:52:24 +0400

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -9,3 +9,5 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     window = ExampleApp()  # Создаём объект класса ExampleApp
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
+    if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
