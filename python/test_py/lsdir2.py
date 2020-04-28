fb7076f9a0b62ffcdf98e6c96409a73e1601dc70
Konstantin <38739711+Mosl1k@users.noreply.github.com>
2020-04-28 22:01:10 +0200

    lsdir ok

--- python/test_py/lsdir2.py
+++ python/test_py/lsdir2.py
@@ -2,73 +2,45 @@
 # -*- coding: utf-8 -*-
 
 import os, sys, time, datetime, filecmp
-
 from os import listdir
-
 from os.path import isfile, join
 
 scan_path=os.getcwd()
-
+obj1 = []
+obj2 = []
+nf1 = []
+nf2 = []
+now = datetime.datetime.now()
 def scan(fa):
-
     print(scan_path + "/" + fa + " ,"+ str(size) + " bytes, " + 'corrected: ' + str(correct))
-
 files1 = [f1 for f1 in listdir(scan_path) if isfile(join(scan_path, f1))]
 
-obj1 = []
-
-obj2 = []
-
 for fa1 in files1:
-
     size = int(os.path.getsize(fa1))
-
     correct = datetime.datetime.fromtimestamp(int(os.path.getmtime(fa1)))
-
     obj = {"fi":str(fa1),"si":str(size),"co":str(correct)}
-
+    na = {"fi":str(fa1),"si":str(size)}
+    nf1.append(na)
     obj1.append(obj)
-    #print(obj["fi"])
-    #print(str(obj)+'\n')
-
-print ('\n\n\n')
-
-time.sleep(5)
-
+print ('\n')
+time.sleep(7)
 files2 = [f2 for f2 in listdir(scan_path) if isfile(join(scan_path, f2))]
-
 for fa2 in files2:
-
     size = int(os.path.getsize(fa2))
     correct=datetime.datetime.fromtimestamp(int(os.path.getmtime(fa2)))
-
     obj = {"fi":str(fa2),"si":str(size),"co":str(correct)}
-    #print(obj)
+    na = {"fi":str(fa2),"si":str(size)}
+    nf2.append(na)
+    obj2.append(obj)
     for fif in obj1:
         if fif["fi"] == obj["fi"]:
             if fif["co"] != obj["co"]:
-                print('fififi:', obj["fi"],obj["co"])		
-#    for fif in obj1:    
-   #     if fif["fi"] not in obj["fi"]:
-  #          print('del:', fif["fi"])
+                print('corrected:', obj["fi"],obj["co"])		
+for obj_add in nf2:
+    if obj_add not in nf1:
+        print('add:', obj_add["fi"],obj_add["si"], str(now))
 
-        
-  #      scan(fa2)
-
-    #    obj2.append(obj)
-    #else: 
-       
-     #   obj2.append(obj)	
-
-#for obj_add in obj2:
-
- #   if obj_add not in obj1:
-
-  #      print('add:', obj_add)
-
-#for obj_del in obj1:
-
- #   if obj_del not in obj2:
-
-#        print('del:', obj_del)
+for obj_del in nf1:
+    if obj_del not in nf2:
+        print('del:', obj_del["fi"],obj_del["si"], str(now))
 
